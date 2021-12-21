e304357a0ae093c6d503a3104ebdc34c0b7a6d2a
Stanley Pan <imoment.developer@gmail.com>
2021-12-21 00:06:13 -0500

    Initial Commit

---
+++ FaceID_Auth/FaceID_AuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceID_AuthApp.swift
+//  FaceID_Auth
+//
+//  Created by Stanley Pan on 2021/12/21.
+//
+
+import SwiftUI
+
+@main
+struct FaceID_AuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
